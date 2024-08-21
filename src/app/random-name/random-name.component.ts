bf0302fd764284143c8668bb20ccf228226e90da
ahmed <ahmedshbeeb98@gmail.com>
2024-08-22 01:44:43 +0300

    add random name generator

--- src/app/random-name/random-name.component.ts
+++ src/app/random-name/random-name.component.ts
@@ -1,10 +1,20 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-random-name',
+  selector: 'random-name',
   templateUrl: './random-name.component.html',
   styleUrl: './random-name.component.css'
 })
 export class RandomNameComponent {
 
+  randomName:string ="rashed"
+
+
+
+  generateRandomName(){
+    let names =["ahmed","omar","mohamed"]
+    let randomNumber= Math.floor(Math.random() * 3);
+    this.randomName = names[randomNumber]
+    console.log(this.randomName);
+  }
 }
